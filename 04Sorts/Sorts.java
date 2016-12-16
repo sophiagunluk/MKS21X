97555ffd68f15bf0dd75b8e96ae1502f7c9f3a85
Sophia Gunluk <sgunluk@stuy.edu>
2016-12-15 23:27:44 -0500

    bubble sort and insertion sort added

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -10,7 +10,7 @@ public class Sorts{
     public Sorts(int[] d){
 	data = d;
 	//selectionSort(data);
-	insertionSort(data);
+	bubbleSort(data);
     }
   
     public static void selectionSort(int[] data){
@@ -39,6 +39,21 @@ public class Sorts{
 		}
     }
 
+    public static void bubbleSort(int[] data){
+        boolean done = false;
+        while (!done){
+        	done = true;
+	        for (int j = 0; j < data.length-1; j++){
+	        	if (data[j+1] < data[j]){
+	            	int swap = data[j+1];
+	            	data[j+1] = data[j];
+	            	data[j] = swap;
+	            	done = false;
+	            }
+	        }
+	    }
+    }
+
     public String toString(){
         String fin = "[";
         for(int i = 0 ;i < data.length - 1; i++){
@@ -49,9 +64,18 @@ public class Sorts{
 
 
     public static void main(String[] arrgg){
-	int[] tester = {64, 25, 12, 22, 11};
-	Sorts test1 = new Sorts(tester);
-	System.out.println(test1);
+	    int[] tester1 = {64, 25, 22, 12, 22, 11};
+	    Sorts test1 = new Sorts(tester1);
+	    System.out.println(test1);
+	    int[] tester2 = {64, 25, 22, 12, 22, 11, -10, 11, 674638230};
+	    Sorts test2 = new Sorts(tester2);
+	    System.out.println(test2);
+	    int[] tester3 = {64, 22, 22, 22, 22, 11};
+	    Sorts test3 = new Sorts(tester3);
+	    System.out.println(test3);
+	    int[] tester4 = {64};
+	    Sorts test4 = new Sorts(tester4);
+	    System.out.println(test4);
     }
 	
 }
